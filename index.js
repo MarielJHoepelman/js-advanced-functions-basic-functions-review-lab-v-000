a5ad29bacc4f97544fbb5ffff06805792e69a87e
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-09-30 20:18:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function wrapAdjective(symbol = "*"){
 // }
 
 const Calculator = {
-  add: function (a,b) {return a + b};
+  add: function (a,b) {return a + b}
   // subtract: function (a,b) {return a - b};
 }
