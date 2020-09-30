a0c36ac3f98584136e5f16d661cb0840756bced3
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-09-30 19:57:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function mondayWork(activity = "go to the office"){
   return `This Monday, I will ${activity}.`
 }
 
-function wrapAdjective(symbol = "*"){
-  return function(adjective){
-    return `You are ${symbol}${adjective}${symbol}!`
-  }
-}
+// function wrapAdjective(symbol = "*"){
+//   return function(adjective){
+//     return `You are ${symbol}${adjective}${symbol}!`
+//   }
+// }
 /*
 
 const wrapAdjective = (symbol = "*") => (adjective) => {
